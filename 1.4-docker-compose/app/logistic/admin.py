195205c8796d23108c9782d473b4e3cee8408d42
root <root@194-58-96-121.cloudvps.regruhosting.ru>
2021-08-18 17:59:38 +0300

    1.4 makeup

--- 1.4-docker-compose/app/logistic/admin.py
+++ 1.4-docker-compose/app/logistic/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Stock, Product
+
+
+class ProductInline(admin.TabularInline):
+    model = Stock.products.through
+    verbose_name = 'Product'
+    verbose_name_plural = 'Products'
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    inlines = [ProductInline,]
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
--- 1.4-docker-compose/app/logistic/models.py
+++ 1.4-docker-compose/app/logistic/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=60, unique=True)
     description = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.title
 
 
 class Stock(models.Model):
