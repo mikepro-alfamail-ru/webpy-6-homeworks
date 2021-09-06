3494e7f1166a78323f6ce8fa9c12b60cd70d0e4b
Mikhail Prozorov <mikepro@alfamail.ru>
2021-09-06 15:41:35 +0300

    2.2 update

--- 2.2-asyncio/main.py
+++ 2.2-asyncio/main.py
@@ -1,77 +1,67 @@
 import asyncio
+import time
+
 import aiosqlite3
 import aiosmtplib
-import json
 
 from more_itertools import chunked
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+MAX_MAILS = 5
+MIN_TIME = 10
+# Ограничения mailtrap.io, 5 писем в 10 секунд
+
 
 MAIL_PARAMS = {
-    'TLS': True,
     'host': 'smtp.mailtrap.io',
     'username': 'aba1a44638c6a8',
     'password': '0ebcc9aa6a92cf',
-    'port': 587,
+    'port': 25,
     'mailfrom': 'test@example.com'
 }
 
-async def db_contacts(loop):
-    async with aiosqlite3.connect('contacts.db', loop=loop) as conn:
+
+async def db_contacts():
+    async with aiosqlite3.connect('contacts.db') as conn:
         async with conn.cursor() as cur:
             await cur.execute("SELECT * FROM contacts;")
             list_contacts = await cur.fetchall()
-            return list_contacts
+    return list_contacts
 
-async def sendmail_async(mailto,name,**params):
+
+async def sendmail_async(mailto, name, **params):
     mail_params = params.get("mail_params", MAIL_PARAMS)
     msg = MIMEMultipart()
-    msg.preamble = "Thanks"
-    msg['Subject'] = "Thanks"
+    msg['Subject'] = "Вот спасибки, так спасибки!"
     msg['From'] = mail_params.get('mailfrom')
     msg['To'] = mailto
-    body = f"Уважаемый {name}! \n" \
-           f"Спасибо, что пользуетесь нашим сервисом объявлений."
+
+    body = f'''Уважаемый {name}!
+Спасибо, что пользуетесь нашим сервисом объявлений.'''
+
     msg.attach(MIMEText(body, 'plain'))
 
     host = mail_params.get('host', 'localhost')
-    isSSL = mail_params.get('SSL', False)
-    isTLS = mail_params.get('TLS', False)
-    port = mail_params.get('port', 465 if isSSL else 25)
-    smtp = aiosmtplib.SMTP(hostname=host, port=port, use_tls=isSSL)
+    port = mail_params.get('port')
+    smtp = aiosmtplib.SMTP(hostname=host, port=port)
     await smtp.connect()
-    if isTLS:
-        await smtp.starttls()
     if 'username' in mail_params:
         await smtp.login(mail_params['username'], mail_params['password'])
-    await smtp.send_message(msg)
+    result = await smtp.send_message(msg)
     await smtp.quit()
+    return result
 
 
 async def main():
-    loop = asyncio.get_event_loop()
-    list_cont = loop.run_until_complete(db_contacts(loop))
+    contacts = await db_contacts()
+    for contacts_chunk in chunked(contacts, MAX_MAILS):
+        sendmail_couroutines = [
+            sendmail_async(mailto=mailto, name=f'{first_name} {last_name}')
+            for _, first_name, last_name, mailto, *other in contacts_chunk]
+        await asyncio.gather(*sendmail_couroutines)
+        time.sleep(MIN_TIME)
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    list_cont = loop.run_until_complete(db_contacts(loop))
-
-    co_list = []
-    for id, first_name, last_name, mailto, *other in list_cont:
-        co1 = sendmail_async(mailto=mailto, name= f'{first_name} {last_name}')
-        co_list.append(co1)
-    loop.run_until_complete(asyncio.gather(*co_list))
-
-    loop.close()
-
-    # print(*list_cont, sep='\n')
-    # print(len(list_cont))
-
-    # co_list = []
-    # for mail in list_cont:
-    #     co1 = send_mail_async(to=mail[3],name= f'{mail[1]} {mail[2]}')
-    #     co_list.append(co1)
-    # loop.run_until_complete(asyncio.gather(*co_list))
-    # loop.close()
+    asyncio.run(main())
